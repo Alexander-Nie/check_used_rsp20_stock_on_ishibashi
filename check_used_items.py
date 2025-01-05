fe260790b90a97a5d2fdd94cb6566ac9dcdb4048
Alexander-Nie <alex_nby@icloud.com>
2025-01-05 17:35:30 +0900

    add log

--- check_used_items.py
+++ check_used_items.py
@@ -3,6 +3,11 @@ from bs4 import BeautifulSoup
 import os
 import datetime
 import pytz
+import logging
+
+# Set up logging with Japan time
+logging.basicConfig(filename='check_used_items.log', level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%m-%d-%Y %H:%M:%S')
+japan_timezone = pytz.timezone('Asia/Tokyo')
 
 def send_telegram_message(message):
     """Send a message to a Telegram user specified by chat ID."""
@@ -11,6 +16,9 @@ def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
     requests.get(url)
 
+def get_japan_time():
+    """Returns the current time in Japan timezone."""
+    return datetime.datetime.now(japan_timezone).strftime('%m-%d-%Y %H:%M:%S')
 
 def check_time_and_send_health_message():
     """Check if the current time is between 14:00 and 15:00 in Japan and send a health check message."""
@@ -23,19 +31,17 @@ def main():
     # First, perform the health check message.
     check_time_and_send_health_message()
 
-
     url = 'https://www.ishibashi.co.jp/ec/search/?PSTA=&PEND=&SWD=YAMAHA%E3%80%80RSP20&EWD=&CAT1=&CAT2=&CAT3=&SRT=3&LMT=24&TSEL=0'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    used_tab = soup.find('div', class_='search_main_list_bar_tab', string=lambda text: '中古' in text if text else False)
-    
+    used_tab = soup.find('div', class_='search_main_list_bar_tab', string=lambda text: '中古' in text if text else False)    
     if used_tab:
         used_count = used_tab.text.split('(')[1].strip(')')
+        logging.info(f"{get_japan_time()}: Used RSP20 has stock: {used_count}")
         if int(used_count) > 0:
             send_telegram_message(f"Usde RSP20 has stock: {used_count}")
-        else:
-            pass
     else:
+        logging.info(f"{get_japan_time()}: No Data: RSP20")
         send_telegram_message("No Data: RSP20")
 
 
